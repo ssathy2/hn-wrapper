1ee2eba94105da778e5df511b41f0502b42320b3
Siddharth Sathyam <ssathy2@gmail.com>
2015-01-29 21:47:53 -0600

    Moo

--- server.js
+++ server.js
@@ -74,6 +74,7 @@ server.get(/.*/, restify.serveStatic({
 }));
 
 var port = process.env.PORT || 5000;
+console.log('Port: %i', port);
 server.listen(port, function () {
  console.log('%s listening at %s', server.name, server.url);
 });
